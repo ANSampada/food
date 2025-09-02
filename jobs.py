91b5dbe10ea59f4316e774959a60effe27efb279
ANSampada <121248549+ANSampada@users.noreply.github.com>
2025-09-02 19:40:31 +0530

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,8 +1,12 @@
+import os
 from jinja2 import Environment, FileSystemLoader
 
 class Job:
     def __init__(self, name, template, **kwargs):
-        env = Environment(loader=FileSystemLoader("templates"))
+        # Find the directory of this file (jobs.py)
+        template_dir = os.path.join(os.path.dirname(__file__), "templates")
+        env = Environment(loader=FileSystemLoader(template_dir))
+        
         tmpl = env.get_template(template)
         self.name = name
         self.config = tmpl.render(**kwargs).encode("utf-8")
