eaa116c0329813b2145ea08978227ce16eddfa33
ANSampada <121248549+ANSampada@users.noreply.github.com>
2025-09-02 16:45:34 +0530

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,1 +1,8 @@
+from jinja2 import Environment, FileSystemLoader
 
+class Job:
+    def __init__(self, name, template, **kwargs):
+        env = Environment(loader=FileSystemLoader("templates"))
+        tmpl = env.get_template(template)
+        self.name = name
+        self.config = tmpl.render(**kwargs).encode("utf-8")
