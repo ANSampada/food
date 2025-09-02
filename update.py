ea34e0937e679a94b351bedcce051704dd40056b
ANSampada <121248549+ANSampada@users.noreply.github.com>
2025-09-02 19:09:45 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,21 @@
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.custom_exceptions import UnknownJob
 
+# Folder XML definition
+FOLDER_XML = """<com.cloudbees.hudson.plugins.folder.Folder plugin="cloudbees-folder">
+  <description>Recipes folder</description>
+</com.cloudbees.hudson.plugins.folder.Folder>"""
+
 def get_jenkins():
     return Jenkins("http://10.209.132.115:8080", username="sampada", password="11e16ad45ee52ec04022655004741c614f")
+
+def ensure_folder(jenkins, folder_name):
+    """Create Jenkins folder if it doesn't exist"""
+    if folder_name not in jenkins.keys():
+        jenkins.create_job(folder_name, FOLDER_XML)
+        print(f" Created folder: {folder_name}")
+    else:
+        print(f" Folder already exists: {folder_name}")
 
 def update_job(job_spec):
     jenkins = get_jenkins()
