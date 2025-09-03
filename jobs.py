646904e5bf8eff6ec10fb5c20a7c91b02d8755f7
ANSampada <121248549+ANSampada@users.noreply.github.com>
2025-09-03 15:47:31 +0530

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -4,7 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 class Job:
     def __init__(self, name, template, **kwargs):
         # Find the directory of this file (jobs.py)
-        template_dir = os.path.join(os.path.dirname(__file__), "templates")
+        template_dir = os.path.join(os.path.dirname(__file__), "templetes")
         env = Environment(loader=FileSystemLoader(template_dir))
         
         tmpl = env.get_template(template)
